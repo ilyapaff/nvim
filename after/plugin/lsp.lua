af46fbcda915414637107c5b89a273b1396c794d
iaparfenov <iaparfenov@lanit.ru>
2023-12-12 18:02:43 +0300

    Обновил lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,11 @@ local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
 local cmp_format = require('lsp-zero').cmp_format()
 local cmp_action = require('lsp-zero').cmp_action()
+local configs = require('lspconfig.configs')
+local lspconfig = require('lspconfig')
+local util = require('lspconfig.util')
 
+lsp_zero.extend_lspconfig()
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
@@ -16,6 +20,7 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
   vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', { buffer = bufnr })
   vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', { buffer = bufnr })
+  vim.keymap.set('n', 'gu', '<cmd>Telescope lsp_document_symbols<cr>', { buffer = bufnr })
 end)
 
 -- Отключаю подсветку
@@ -28,7 +33,7 @@ end)
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls" },
+  ensure_installed = { "lua_ls", "gopls" },
 
   pip = {
     ---@since 1.0.0
@@ -66,6 +71,25 @@ require("mason-lspconfig").setup({
         end,
       })
     end,
+    helm_ls = function()
+      if not configs.helm_ls then
+        configs.helm_ls = {
+          default_config = {
+            cmd = {"helm_ls", "serve"},
+            filetypes = {'helm'},
+            root_dir = function(fname)
+              return util.root_pattern('Chart.yaml')(fname)
+            end,
+          },
+        }
+      end
+
+      lspconfig.helm_ls.setup {
+        filetypes = {"helm"},
+        cmd = {"helm_ls", "serve"},
+      }
+
+    end
   },
 })
 
@@ -95,3 +119,5 @@ cmp.setup({
   --- (Optional) Show source name in completion menu
   formatting = cmp_format,
 })
+
+
